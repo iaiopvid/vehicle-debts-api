b4576030b31129d077714ab6ee1e76e45325a940
iaiopvid <iaiopvid34a@gmail.com>
2026-05-13 09:18:19 -0300

    test: update Jest configuration and add end-to-end tests for debt consultation

--- test/debts.e2e-spec.ts
+++ test/debts.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 
 describe('Debts E2E', () => {
   let app: INestApplication;
